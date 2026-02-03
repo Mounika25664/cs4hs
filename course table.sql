705694f1a4e4fc32c59fa0bc716e2143b32b2096
Mounika25664 <vtu25664@veltech.edu.in>
2026-02-03 20:32:19 +0530

    Add files via upload

---
+++ course table.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: 
+-- Date: 2026-02-03 11:53
+*/
+INSERT INTO `` (`course code`,`course name`,`faculty id`,`student id`,`faculty email`) VALUES (CS101,'CN','FCS01','VTU001','fcs01@gmail.com');
+INSERT INTO `` (`course code`,`course name`,`faculty id`,`student id`,`faculty email`) VALUES (CS102,'JAVA','FCS02','VTU002','fcs02@gmail.com');
+INSERT INTO `` (`course code`,`course name`,`faculty id`,`student id`,`faculty email`) VALUES (CS103,'HTML','FCS03','VTU003','fcs03@gmail.com');
+INSERT INTO `` (`course code`,`course name`,`faculty id`,`student id`,`faculty email`) VALUES (CS104,'DBMS','FCS04','VTU004','fcs04@gmail.com');
+INSERT INTO `` (`course code`,`course name`,`faculty id`,`student id`,`faculty email`) VALUES (CS105,'OS','FCS05','VTU005','fcs01@gmail.com');
---
+++ student table.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: 
+-- Date: 2026-02-03 11:33
+*/
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone number`,`department`) VALUES (vtu1,'mounika','mouni@gmail.com','6584753184','CSE(CS)');
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone number`,`department`) VALUES (vtu2,'sharanya','sharu@gmail.com','8753168418','CSE(CS)');
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone number`,`department`) VALUES (vtu3,'varsha','varsha@gmail.com','9784516546','CSE(CS)');
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone number`,`department`) VALUES (vtu4,'navya','navya@gmail.com','8764316842','CSE(CS)');
+INSERT INTO `` (`vtu number`,`name`,`email`,`phone number`,`department`) VALUES (vtu5,'likitha','likitha@gmail.com','8153934341','CSE(CS)');
